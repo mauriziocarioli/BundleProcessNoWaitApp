1e77bf00ff8254c8a9d76b84e0e073de498f4af4
mauriziocarioli <maurizio.carioli@gmail.com>
2019-06-08 19:23:36 -0400

    initial commit

---
+++ BundleProcessNoWait-model/src/main/java/com/insurance/handler/SendTaskHandler.java
@@ -0,0 +1,27 @@
+package com.insurance.handler;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+public class SendTaskHandler implements WorkItemHandler {
+    
+    private static final Logger logger = LoggerFactory.getLogger(SendTaskHandler.class);
+
+    @Override
+    public  void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+
+        String message = (String) workItem.getParameter("Message");
+        //logger.debug("Sending message: {}", message);
+        manager.completeWorkItem(workItem.getId(), null);
+
+    }
+
+    @Override
+    public  void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+        // Do nothing, cannot be aborted
+    }
+
+}
